f606accba679a52d454364638e90b2154f427400
daler <bobojonovad46@gmail.com>
2023-11-19 22:38:17 +0300

    Добавьте файлы проекта.

---
+++ pract8/pract8.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b = 5;
+	cout << "Введите число: ";
+	cin >> a;
+	b += 5;
+	if (a % 2 == 0) {
+		cout << "Число четное!";
+	}
+	else
+	{
+		cout << "Число нечетное!";
+	}
+	cout << b;
+}
+
